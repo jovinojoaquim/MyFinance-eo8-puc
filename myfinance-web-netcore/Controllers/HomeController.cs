3cb2e41da26626c1b3d5a1ebaf2762d0f2b360b4
Jovino Joaquim <jovinojoaquim@hotmail.com>
2025-05-19 23:09:44 -0300

    Adicionando a chamada a pagina PUC

--- myfinance-web-netcore/Controllers/HomeController.cs
+++ myfinance-web-netcore/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult PUC()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
